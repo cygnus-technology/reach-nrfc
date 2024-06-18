a4ff439bc30c1b012e5e4b5a90c1390f79ec76db
joseph_peplinski <joseph.peplinski@i3pd.com>
2024-06-18 09:15:01 -0500

    Changed app version to a minor version change (v0.2.0) rather than a major version change(v1.0.0)

--- include/app_version.h
+++ include/app_version.h
@@ -7,8 +7,8 @@
 #ifndef _APP_VERSION_H_
 #define _APP_VERSION_H_
 
-#define APP_MAJOR_VERSION 1
-#define APP_MINOR_VERSION 0
+#define APP_MAJOR_VERSION 0
+#define APP_MINOR_VERSION 2
 #define APP_PATCH_VERSION 0
 
 // appends a note string to the version
